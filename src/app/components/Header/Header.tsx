30048c68c23bced2a3f1e3e10010a11d7bc0db15
peschke <mr.peschke@gmail.com>
2025-12-05 22:23:29 -0700

    figured out a fix for that header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,14 +2,15 @@ import './Header.css'
 import Marquee from "./components/Marquee"
 
 export default function Header() {
+    const part1 = "Health insurance that".split(' ')
+    const part2 = "doesn't get in".split(' ')
+    const part3 = "the way.".split(' ')
     return (
-        <div className="header-component green500 flex-center">
-            <h1>
-                Health insurance that 
-                <em> doesn't get in </em>
-                <Marquee />
-                <em> the way.</em>
-            </h1>
+        <div className="header-component green500 flex-center flex-wrap">
+            {part1.map((part, index) => <h1 key={index}>{part}</h1>)}
+            {part2.map((part, index) => <h1 key={index} className='turn-green-placeholder'>{part}</h1>)}
+            <Marquee />
+            {part3.map((part, index) => <h1 key={index} className='turn-green-placeholder'>{part}</h1>)}
         </div>
     )
 }
