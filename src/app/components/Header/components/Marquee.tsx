8b076ede74f20cb9a2480c0cc52cfbdb1ed753dd
peschke <mr.peschke@gmail.com>
2025-12-06 08:00:30 -0700

    got gsap working with some basic functionality

--- src/app/components/Header/components/Marquee.tsx
+++ src/app/components/Header/components/Marquee.tsx
@@ -1,26 +1,47 @@
+'use client'
+
+import './Marquee.css'
 import Image from "next/image";
 import idCardIcon from '../../../../../public/images/id-card-icon.svg'
 import { Fragment } from "react/jsx-runtime";
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export default function Marquee() {
     const complaints = [
+        "claim denials",
+        "frustrated users",
         "unpredictable rate increases",
         "lack of transparency",
         "implementation headaches",
         "claim denials",
-        "frustrated users"
+        "frustrated users",
+        "unpredictable rate increases",
+        "lack of transparency",
+        "implementation headaches"
     ]
 
+    gsap.registerPlugin(useGSAP);
+
+    useGSAP(() => {
+        gsap.to(".marquee-item-container", { xPercent: -100, repeat: -1, duration: 10, ease: "linear" }).totalProgress(0.5);
+        gsap.set(".marquee-moving-stripe", { xPercent: -50 });
+    });
+
     return (
-        <span className="marquee-component green100-border">
-            {complaints.map((complaint) => {
-                return (
-                    <Fragment key={complaint}>
-                        <p className="title orange">{complaint}</p>
-                        <Image src={idCardIcon} alt={"id card icon"} />
-                    </Fragment>
-                )
-            })}
-        </span>
+        <div className="marquee-component green100-border">
+            <div className='marquee-moving-stripe'>
+                <div className='marquee-item-container'>
+                    {complaints.map((complaint, index) => {
+                        return (
+                            <Fragment key={index}>
+                                <p className="marquee-item title orange">{complaint}</p>
+                                <Image className='marquee-item' src={idCardIcon} alt={"id card icon"} />
+                            </Fragment>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
