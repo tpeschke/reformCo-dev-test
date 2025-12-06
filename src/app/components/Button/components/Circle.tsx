e37f02d5c9c3155a75406c1f785d70834cf2eecf
peschke <mr.peschke@gmail.com>
2025-12-06 08:44:21 -0700

    refactoring image folder

--- src/app/components/Button/components/Circle.tsx
+++ src/app/components/Button/components/Circle.tsx
@@ -1,6 +1,6 @@
 import './Circle.css'
 import Image from "next/image";
-import arrowGreen400 from "../../../../../public/images/arrow-circle-green-400.svg"
+import arrowGreen400 from "../../../../../public/images/arrows/arrow-circle-green-400.svg"
 
 export default function Circle() {
     {/* Note to self: the color of the arrow doesn't change until after its fully behind the button so you can just do a 
