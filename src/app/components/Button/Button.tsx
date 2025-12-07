a5a67c6f686bd2e996006edf53f00532720bce88
peschke <mr.peschke@gmail.com>
2025-12-07 12:07:34 -0700

    got the basic button switching functionality down

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,11 +1,29 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import './Button.css'
 import Circle from './components/Circle'
+import gsap from 'gsap';
 
 export default function Button() {
+    const [activeTagIndex, setActiveTagIndex] = useState<0 | 1>(0)
+
+    const swapButtons = () => {
+        setActiveTagIndex(1)
+        gsap.to(".circle-arrow-button", { x: -233 })
+        gsap.to("#main-button", { x: 58, color: '#00B684', border: '1px solid #00B684' })
+    }
+
+    const resetButtons = () => {
+        setActiveTagIndex(0)
+        gsap.to(".circle-arrow-button", { x: 0 })
+        gsap.to("#main-button", { x: 0, color: '#30715D', border: '1px solid #30715D' })
+    }
+
     return (
-        <div className="button-component">
-            <button className="green400">Get a Custom Quote Today</button>
-            <Circle />
+        <div onClick={_ => alert("Your Quote: $1,000,000 for of 6 discounted band-aids a year.")} className="button-component" onMouseOver={swapButtons} onMouseOut={resetButtons}>
+            <button id='main-button' className="green400">Get a Custom Quote Today</button>
+            <Circle activeTagIndex={activeTagIndex} />
         </div>
     )
 }
--- src/app/components/Button/components/Circle.tsx
+++ src/app/components/Button/components/Circle.tsx
@@ -1,11 +1,19 @@
 import './Circle.css'
 import Image from "next/image";
 import arrowGreen400 from "../../../../../public/images/arrows/arrow-circle-green-400.svg"
+import arrowGreen200 from "../../../../../public/images/arrows/arrow-circle-green-200.svg"
 
-export default function Circle() {
-    {/* Note to self: the color of the arrow doesn't change until after its fully behind the button so you can just do a 
-        quick swap */}
-    return (
-        <Image src={arrowGreen400} alt={"id card icon"} />
-    )
+interface Props {
+    activeTagIndex: 0 | 1
+}
+
+export default function Circle({ activeTagIndex }: Props) {
+    switch (activeTagIndex) {
+        case 0:
+            return <Image className='circle-arrow-button' src={arrowGreen400} alt={"id card icon"} />
+        case 1:
+            return <Image className='circle-arrow-button' src={arrowGreen200} alt={"id card icon"} />
+        default:
+            return <p>Something Went Wrong</p>
+    }
 }
